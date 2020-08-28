59f16ab408857654dd9408ca0d5ed51c62c556a6
sclurb <sclurb@outlook.com>
2020-08-27 21:13:28 -0400

    Finished Module-3

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+        public IRestaurantData RestaurantData { get; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            RestaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = RestaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -14,6 +14,8 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
@@ -25,8 +27,10 @@ namespace OdeToFood.Pages.Restaurants
         }
         public void OnGet()
         {
+
+            
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
