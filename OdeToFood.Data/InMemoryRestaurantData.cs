edaefa302441f6d6f0eb69d30207c7583b3a49f5
sclurb <sclurb@outlook.com>
2020-08-30 15:10:00 -0400

    Completed Module-6

--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -65,5 +65,10 @@ namespace OdeToFood.Data
             }
             return restaurant;
         }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
     }
 }
