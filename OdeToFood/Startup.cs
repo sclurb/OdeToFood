84c1ff417c70db57814cefe6f367fb5ff2a54174
sclurb <sclurb@outlook.com>
2020-08-30 21:59:51 -0400

    Completed Module-7

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using OdeToFood.Data;
 
 namespace OdeToFood
@@ -43,6 +36,7 @@ namespace OdeToFood
             });
 
             services.AddRazorPages();
+            services.AddControllers();
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -62,6 +56,7 @@ namespace OdeToFood
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules();
             app.UseCookiePolicy();
 
             //app.UseMvc();
@@ -69,6 +64,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
 
         }
